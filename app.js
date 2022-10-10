63515c7d714f30ee1d3937cc8d99faba9b561b69
nillya7 <112470142+nillya7@users.noreply.github.com>
2022-10-10 15:58:56 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,88 +1,72 @@
+// things to fix:
+// callback functions and restructure the code
+// remove stocks from being an optuin (or find a solution that won't crash the browser)
+
+
+// things to add:
+// loader while the results are loading
+// on re-clicking: clear the results and create new one
+// infographic of all insturments and how many etoro has to offer (stocks 5000, indices 21 etc...)
+
+
 
 const xhr = new XMLHttpRequest();
+
+// get all html elements
 const divDataArea = document.getElementById('data-area')
+const divCountInstruments = document.getElementById('divCountInstruments')
+const spanInstToInvestNum = document.getElementById('spanInstToInvestNum')
+const divCountInstrumentTypes = document.getElementById('divCountInstrumentTypes')
+const spanInvestTypeNum = document.getElementById('spanInvestTypeNum')
+const selectAreaDiv = document.getElementById('selectAreaDiv')
+const selectDiv = document.getElementById('selectDiv')
+const selectInstType = document.getElementById('selectInstType')
+const showInstrumentListBtn = document.getElementById('showInstrumentListBtn')
+
+// an object of Instrument Types
+const instTypeObj = [
+    {
+        instType: 1,
+        instTypeName: "Currencies"
+    },
+    {
+        instType: 2,
+        instTypeName: "Commodities"
+    },
+    {
+        instType: 4,
+        instTypeName: "Indices"
+    },
+    {
+        instType: 5,
+        instTypeName: "Stocks"
+    },
+    {
+        instType: 6,
+        instTypeName: "ETFs"
+    },
+    {
+        instType: 10,
+        instTypeName: "Crypto currencies"
+    },
+];
 
 
 // Create the data function
-function createEtoroSummary(data) {
+function createEtoroSummary(data, uniqueInstTypesArray) {
+    
     // count of instruments
-    let divCountInstruments = document.createElement('div');
-    divCountInstruments.innerText = "eToro offers " + data.length + " instruments to invest"
-    divCountInstruments.id = "divCountInstruments";
-    divDataArea.appendChild(divCountInstruments);
-
-    // unique array of instrument types
-    let instTypes = data.map(instTypes => instTypes.InstrumentTypeID)
-    let instTypesArray = [];
-    instTypesArray.push(instTypes);
-    let uniqueInstTypesArray = [...new Set(instTypesArray[0])];
-    //console.log(uniqueInstTypesArray)
-    
-    // count of instrument types
-    let divCountInstrumentTypes = document.createElement('div');
-    divCountInstrumentTypes.innerText = "in " + uniqueInstTypesArray.length + " instrument types"
-    divCountInstrumentTypes.id = "divCountInstrumentTypes";
-    divDataArea.appendChild(divCountInstrumentTypes);
-
-    // create an object of Instrument Types
-    const instTypeObj = [
-        {
-            instType: 1,
-            instTypeName: "Currencies"
-        },
-        {
-            instType: 2,
-            instTypeName: "Commodities"
-        },
-        {
-            instType: 4,
-            instTypeName: "Indices"
-        },
-        {
-            instType: 5,
-            instTypeName: "Stocks"
-        },
-        {
-            instType: 6,
-            instTypeName: "ETFs"
-        },
-        {
-            instType: 10,
-            instTypeName: "Crypto currencies"
-        },
-    ];
-
-    //console.log(instTypeObj[1].instTypeName)
-    
-    // select the instrument type and show all instruments
-    const selectAreaDiv = document.createElement('div');
-    selectAreaDiv.id = "selectAreaDiv"
-    const selectDiv = document.createElement('div');
-    selectDiv.innerText = "Select the instrument type to display all instruments available to invest:"
-    selectDiv.id = "selectDiv"
-    const selectInstType = document.createElement('select');
-    
-    for (let i=0; i<instTypeObj.length; i++) {
-        const optionInstType = document.createElement('option');
-        optionInstType.innerText = instTypeObj[i].instTypeName
-        selectInstType.appendChild(optionInstType);
-    }
-
-    document.body.appendChild(selectAreaDiv);
-    selectAreaDiv.appendChild(selectDiv);
-    selectAreaDiv.appendChild(selectInstType);
-
-    
-    // display all instruments under specifit instrument type selected
-    const showInstrumentListBtn = document.createElement('button');
-    showInstrumentListBtn.innerHTML = "Show Instruments"
-    showInstrumentListBtn.id = "showInstrumentListBtn"
-    selectAreaDiv.appendChild(showInstrumentListBtn);
-   
-    
+    spanInstToInvestNum.innerText = data.length
         
+    // count of instrument types
+    spanInvestTypeNum.innerText = uniqueInstTypesArray.length
+      
+    
+    // select the instrument type and show all instruments  
+    createSelectOptions();
+ 
+    //alert before choose Stocks
     selectInstType.addEventListener ('change', function() {
-        //alert before choose Stocks
         let checkStocks = selectInstType.options[selectInstType.selectedIndex].value;
             if(checkStocks === "Stocks") {
             alert("this is going to crash!")
@@ -92,18 +76,27 @@ function createEtoroSummary(data) {
 
     showInstrumentListBtn.addEventListener ('click', getInstrumentsBasedOnType);
     
-
     function getInstrumentsBasedOnType() {
-        let selectedInstTypeValue = selectInstType.options[selectInstType.selectedIndex].value;
+        
+        // loader
+        // const loaderDiv = document.createElement('div');
+        // loaderDiv.innerText = "loading";
+        // document.body.appendChild(loaderDiv)
+
+
         // get the instrument display name
+        let selectedInstTypeValue = selectInstType.options[selectInstType.selectedIndex].value;
+        
         // replace Instrument Type name the user selected to Intrument Type value
+        //getInstrumentTypeName();
         for (let i=0; i<instTypeObj.length; i++) {
             if(instTypeObj[i].instTypeName === selectedInstTypeValue) {
                 var instTypeValue = instTypeObj[i].instType                 // the instrument type value the user selcted
             }
         }
 
-        // create listOfInst -- which is an array includes the instrument display name based on the instrument type the user selected
+        // create listOfInst Array -- includes the Instrument Display Name (based on the instrument type the user selected)
+        // createLisfOfInstArray(data, instTypeValue);
         for (let i=0; i<data.length; i++) {
             if(data[i].InstrumentTypeID === instTypeValue) {
                 var listOfInst = data
@@ -111,12 +104,15 @@ function createEtoroSummary(data) {
                 .map(listOfInst => listOfInst.InstrumentDisplayName)
             }
         }
+
+        //create the results area
+        //createResultsAres(listOfInst, selectedInstTypeValue)
         const divResults = document.createElement('div');
         divResults.id = "divResults";
 
         const divCount = document.createElement('div');
         divCount.id = "divCount"
-        divCount.innerText = listOfInst.length + " " + selectedInstTypeValue + " available to invest:"
+        divCount.innerText = listOfInst.length + " " + selectedInstTypeValue + " available:"
 
         document.body.appendChild(divResults);
         divResults.appendChild(divCount);
@@ -126,13 +122,14 @@ function createEtoroSummary(data) {
         const trHeader = document.createElement('tr');
         const th1 = document.createElement('th');
         const th2 = document.createElement('th');
-        // th1.innerText = "1";
-        th2.innerText = "Sort";
+        // th2.innerText = "Sort";
         trHeader.appendChild(th1);
         trHeader.appendChild(th2);
         tableOfInst.appendChild(trHeader)
 
-        //get the SymbolFull
+
+        //get the SymbolFull (for the link)
+        //getSymbolFull(data, instTypeValue);
         for (let i=0; i<data.length; i++) {
             if(data[i].InstrumentTypeID === instTypeValue) {
                 var symbolFull = data
@@ -142,6 +139,7 @@ function createEtoroSummary(data) {
         };
     
         //get the images
+        //getImages(data, instTypeValue);
         let instrumentImageArray = []
         for (let i=0; i<data.length; i++) {
             if(data[i].InstrumentTypeID === instTypeValue) {
@@ -151,6 +149,8 @@ function createEtoroSummary(data) {
         }
         
         //create rows in the table based on the user's selection
+        //createDynamicTable(listOfInst, data, symbolFull, instrumentImageArray, tableOfInst, divResults)
+
         for (let i=0; i<listOfInst.length; i++) {
             var tr = document.createElement('tr');
              
@@ -169,7 +169,7 @@ function createEtoroSummary(data) {
                 // image of the instument in another cell of the table
                 const img = document.createElement('img');
                 img.src = instrumentImageArray[i];
-                var tdImage = document.createElement('td'); // add image to another td -- breaks the browser!
+                var tdImage = document.createElement('td');
                 tdImage.appendChild(img);
             }
 
@@ -178,14 +178,26 @@ function createEtoroSummary(data) {
             tr.appendChild(tdInstName);
             tableOfInst.appendChild(tr)
             divResults.appendChild(tableOfInst);
-
-            // console.log(tableOfInst.length)
-            // for (let i=0 ; i<tableOfInst.length ; i++) {
-            //     table = tableOfInst[i];
-            // }
         }
     }    
 }
+
+function createArray(data, callback) {
+    let instTypes = data.map(instTypes => instTypes.InstrumentTypeID)
+    let instTypesArray = [];
+    instTypesArray.push(instTypes);
+    let uniqueInstTypesArray = [...new Set(instTypesArray[0])];
+    callback(data, uniqueInstTypesArray);
+}
+
+function createSelectOptions() {
+    for (let i=0; i<instTypeObj.length; i++) {
+        const optionInstType = document.createElement('option');
+        optionInstType.innerText = instTypeObj[i].instTypeName
+        selectInstType.appendChild(optionInstType);
+    }
+}
+
 
 
 
@@ -195,8 +207,7 @@ xhr.onreadystatechange = function() {
         let pardedResponse = JSON.parse(this.responseText);                  // object of the data
         let data = Object.values(pardedResponse)[0]                         // Array of the data
         .filter(countInst => countInst.IsInternalInstrument === false)       //filter to only external instruments
-        
-        createEtoroSummary(data);
+         
         console.log(data) 
         // console.log(data[1]) // specific instrument
         // console.log(data[1].SymbolFull)
@@ -204,9 +215,17 @@ xhr.onreadystatechange = function() {
         // console.log(data[1].InstrumentDisplayName)
         // console.log(data[1].IsInternalInstrument) // should be false
         // console.log(Object.values(data[1].Images[0])[3]) // image of specific instrument [1] place
+
+        createArray(data, createEtoroSummary);
+
+
     }
 }
 
 // API details
 xhr.open('get', 'https://meta.etoro.com/v1.1/instruments.json');
 xhr.send();
+
+
+
+/////////
